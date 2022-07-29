2fcf3cf2a3dd6f6899254017b095c3a15c090379
HiiWee <wpdlzhf159@gmail.com>
2022-07-30 04:11:56 +0900

    타임리프 - 기본 기능: 블록

--- mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -110,6 +110,12 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
     private void addUsers(Model model) {
         ArrayList<User> list = new ArrayList<>();
         list.add(new User("UserA", 10));
