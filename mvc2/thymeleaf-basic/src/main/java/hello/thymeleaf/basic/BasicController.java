2f5ce8c94d9c87841cf34d29f5c0a7f48f4f0be0
HiiWee <wpdlzhf159@gmail.com>
2022-07-30 03:42:47 +0900

    타임리프 - 기본 기능: 조건부 평가

--- mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -98,6 +98,12 @@ public class BasicController {
         return "basic/each";
     }
 
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
     private void addUsers(Model model) {
         ArrayList<User> list = new ArrayList<>();
         list.add(new User("UserA", 10));
