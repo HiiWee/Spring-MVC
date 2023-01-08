9419fc21ec8accaf9f328b504eaa720e8ecc4eb1
HiiWee <wpdlzhf159@gmail.com>
2023-01-08 21:48:54 +0900

    refactor: interface 상수 class 상수로 변경

--- mvc2/login/src/main/java/hello/login/web/SessionConst.java
+++ mvc2/login/src/main/java/hello/login/web/SessionConst.java
@@ -1,5 +1,8 @@
 package hello.login.web;
 
-public interface SessionConst {
-    String LOGIN_MEMBER = "loginMember";
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+
+    private SessionConst() {
+    }
 }
